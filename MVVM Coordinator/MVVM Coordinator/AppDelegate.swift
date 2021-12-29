1bbd0a4c7d93434ca926efb25a35f1e963500fd4
Mehrdad Ahmadian <mehrdad.ahmadian@gmail.com>
2021-12-28 20:42:49 -0500

    Remove the SceneDelegate and Setup AppDelegate

--- MVVM Coordinator/MVVM Coordinator/AppDelegate.swift
+++ MVVM Coordinator/MVVM Coordinator/AppDelegate.swift
@@ -10,10 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let navigationController = UINavigationController.init()
+        appCoordinator = AppCoordinator(navigationController: navigationController)
+        appCoordinator!.start()
+
+        window!.rootViewController = navigationController
+        window!.makeKeyAndVisible()
         return true
     }
 
